719b09aa231aec73d82d93583ee1ff36b52a54bd
abhijitshaw88 <40785748+abhijitshaw88@users.noreply.github.com>
2020-04-04 20:23:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // http://api.openweathermap.org/data/2.5/find?q=London&APPID=7cab49ec801a8dea8afbb713dc26496d
-
-var rqst=new XMLHttpRequest();
+document.getElementById("button").addEventListener('click',loadText)
+function loadText(){
+    console.log("i am clicked");
+}
+    var rqst=new XMLHttpRequest();
 rqst.open('GET','http://api.openweathermap.org/data/2.5/find?q=London&APPID=7cab49ec801a8dea8afbb713dc26496d');
 console.log(rqst.status);
 console.log(rqst.readyState);
@@ -8,6 +11,6 @@ rqst.onload=function(){
     console.log(rqst.readyState);
     var mydata=JSON.parse(rqst.responseText);
    rqst.send();
-};
+}
 console.log(rqst.readyState);
 console.log("hello");
