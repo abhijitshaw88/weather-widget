b5e3e3765da26d8447eaa8910ed638f412cf5124
abhijitshaw88 <40785748+abhijitshaw88@users.noreply.github.com>
2020-04-06 12:28:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 // http://api.openweathermap.org/data/2.5/find?q=London&APPID=7cab49ec801a8dea8afbb713dc26496d
-document.getElementById("button").addEventListener('click',loadText)
+
+//document.getElementById("button").addEventListener('click',loadText);
+
+document.getElementById("mycity").addEventListener('search',loadText);
 function loadText(){
-    console.log("i am clicked");
+    GETDATA();
 }
-    var rqst=new XMLHttpRequest();
-rqst.open('GET','http://api.openweathermap.org/data/2.5/find?q=London&APPID=7cab49ec801a8dea8afbb713dc26496d');
-console.log(rqst.status);
-console.log(rqst.readyState);
-rqst.onload=function(){
-    console.log(rqst.readyState);
-    var mydata=JSON.parse(rqst.responseText);
-   rqst.send();
+
+// var api_d= 'http://api.openweathermap.org/data/2.5/find?q=LONDON&APPID=7cab49ec801a8dea8afbb713dc26496d&units=metric';
+// var str=document.getElementById("mycity").value;
+// var api_d1 = api_d.replace(/LONDON/, document.getElementById("mycity").value);
+
+async  function GETDATA()
+{
+
+    var api_d='http://api.openweathermap.org/data/2.5/find?q=LONDON&APPID=7cab49ec801a8dea8afbb713dc26496d&units=metric';
+     str=document.getElementById("mycity").value;
+    const api_d1 = api_d.replace(/LONDON/,str);
+    // console.log(str);
+
+    /*    ==================== CALLING THE API =================  */
+
+    const response= await fetch(api_d1);
+    const data= await response.json();
+    //document.getElementById("tem1").value='he';
+    document.getElementById("cityname").innerHTML = str ;
+    document.getElementById("tem1").innerHTML = data.list[0].main.temp.toFixed(0);
+    document.getElementById("tem2").innerHTML = data.list[1].main.temp.toFixed(0);
+    document.getElementById("tem3").innerHTML = data.list[2].main.temp.toFixed(0);
+    document.getElementById("tem4").innerHTML = data.list[3].main.temp.toFixed(0);
+    document.getElementById("tem5").innerHTML = data.list[4].main.temp.toFixed(0);
+    //console.log(data.list[1].main.temp.toFixed(0));
 }
-console.log(rqst.readyState);
-console.log("hello");
